06d81b5ce3a0c38dd6b9c5b7910bcda88fa37d10
Hector Ulloa <ulloazhector@gmail.com>
2022-11-10 23:06:27 -0300

    ADD, members in model

--- src/components/pages/members/MemberItem.jsx
+++ src/components/pages/members/MemberItem.jsx
@@ -1,9 +1,19 @@
-export const MemberItem = ({member}) => {
+import { Col } from "react-bootstrap"
+
+export const MemberItem = ({ member }) => {
     return (
-        <div>
-            <h4>{member.name}</h4>
-            <img src={require('../../../assets/images/' + member.image)} alt={member.name} />
-            <p>{member.role}</p>
-        </div>
+        <Col lg={6} xxl={4}>
+            <div className='d-flex align-items-center gap-3'>
+                <img
+                    src={require('../../../assets/images/' + member.image)}
+                    alt={member.name}
+                    style={{ height: 180, width: 180, objectFit: 'cover', borderRadius: '100%' }}
+                />
+                <div>
+                    <h4>{member.name}</h4>
+                    <p>{member.role}</p>
+                </div>
+            </div>
+        </Col>
     )
 }
--- src/components/pages/members/Members.jsx
+++ src/components/pages/members/Members.jsx
@@ -1,20 +1,24 @@
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import members from '../../../models/members.json'
 import { MemberItem } from './MemberItem'
 
 export const Members = () => {
     return (
         <Container>
-            <h2>Equipo</h2>
-            <p>
+            <h2 className='mb-4'>Equipo</h2>
+            <p className='mb-4'>
                 Somos un equipo de trabajo con expertos en Física, Computación, Electrónica y Ciencias de la atmósfera. Nuestros intereses confluyen en este trabajo, donde nuestros trabajos científicos se aplican a un problema relacionado al medio ambiente utilizando tecnologías de punta.
                 Abordamos la problemática desde distintos puntos de vista, y esto incrementa la creatividad de nuestras soluciones y las bases científicas que las sustentan.
                 Los integrantes del equipo de trabajo trabajamos en distintas instituciones: Consejo Nacional de Investigaciones Cietíficas y Técnicas (CONICET), Universidad Nacional de Río Negro (Sede Andina, Bariloche), Centro Atómico Bariloche, Universidad Nacional del Comahue (Centro Regional Universitario Bariloche).
             </p>
-            <h3>Miembros actuales</h3>
-            {members.map(member =>
-                <MemberItem key={member.name} member={member} />
-            )}
+            <Row className='mb-4'>
+                <h3>Miembros actuales</h3>
+            </Row>
+            <Row className='g-4'>
+                {members.map(member =>
+                    <MemberItem key={member.name} member={member} />
+                )}
+            </Row>
         </Container>
     )
 }
