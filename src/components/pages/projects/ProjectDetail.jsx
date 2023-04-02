379af71d1ebc7fcfe703bbd7cf70e2b8070d4acc
Hector Ulloa <ulloazhector@gmail.com>
2023-04-02 16:21:35 -0300

    IMPROVE, videos display

--- src/components/pages/projects/ProjectDetail.jsx
+++ src/components/pages/projects/ProjectDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { Container } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { getProjectById, getUserById } from '../../../utils/api'
 
@@ -24,15 +25,25 @@ export const ProjectDetail = () => {
         return <p>Loading...</p>
 
     return (
-        <div>
+        <Container>
             <h3>{project.name}</h3>
             <br />
 
             <strong>Integrantes</strong>
-            {authors?.map((author,i) =>
+            {authors?.map((author, i) =>
                 <p key={i}>{author.name}</p>
             )}
-            {project?.videos?.map(video => <a href={video}>video </a>)}
-        </div>
+            {project?.videos?.map(video =>
+                <iframe
+                    width="560"
+                    height="315"
+                    src={`https://www.youtube.com/embed/${video.url}`}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                />
+            )}
+        </Container>
     )
 }
